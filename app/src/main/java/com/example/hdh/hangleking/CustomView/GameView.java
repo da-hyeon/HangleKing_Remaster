543469aaa17a31d69882b2e687056bfc08d19bc1
da-hyeon <kjg123kg@gmail.com>
2019-01-18 03:17:00 +0900

    first Commit

--- app/src/main/java/com/example/hdh/hangleking/CustomView/GameView.java
+++ app/src/main/java/com/example/hdh/hangleking/CustomView/GameView.java
@@ -2,7 +2,6 @@ package com.example.hdh.hangleking.CustomView;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
